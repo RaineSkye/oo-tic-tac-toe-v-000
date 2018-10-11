8044f470548611d6dab8367e3ec094cad611e3aa
kitte anderson <kitteanderson@gmail.com>
2018-10-11 19:32:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
     @board[index] = current_player
   end
 
-  def position_taken?(@board,index)
+  def position_taken?(board,index)
     @board[index]== "X" || @board[index]== "O"
   end
 
