ab72779285afb37d9c610f71381a05ebbe1baf07
kitte anderson <kitteanderson@gmail.com>
2018-10-11 19:43:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,5 +100,14 @@ def winner
   end
 end
 
+def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
 
 end
